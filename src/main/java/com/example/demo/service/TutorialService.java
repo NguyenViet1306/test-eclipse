c6a929a3bc4b4a0922dada8e9c7dca0ecf99aaf6
viet <vietsongtu@gmail.com>
2022-12-24 15:21:07 +0700

    first

--- src/main/java/com/example/demo/service/TutorialService.java
+++ src/main/java/com/example/demo/service/TutorialService.java
@@ -1,14 +1,41 @@
 package com.example.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.entity.Tutorial;
 import com.example.demo.repository.TutorialRepo;
 
-@Service
-public class TutorialService {
+public class TutorialService implements IService	
+<Tutorial> {
+	
 	@Autowired
 	TutorialRepo tutorialRepo;
-	
-	
+
+	@Override
+	public List<Tutorial> findAll() {
+		// TODO Auto-generated method stub
+		return tutorialRepo.findAll();
+	}
+
+	@Override
+	public Optional<Tutorial> findById(long id) {
+		// TODO Auto-generated method stub
+		return tutorialRepo.findById(id);
+	}
+
+	@Override
+	public Tutorial save(Tutorial e) {
+		// TODO Auto-generated method stub
+		return tutorialRepo.save(e);
+	}
+
+	@Override
+	public void delete(long id) {
+		// TODO Auto-generated method stub
+		tutorialRepo.deleteById(id);
+	}
+
 }
